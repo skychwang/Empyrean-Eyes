4788c3a3d8726fd13871affd8df9af521b7c2989
skywang329 <sky.wang@yahoo.com>
2017-07-06 15:47:22 +0800

    swift init status bar app

---
+++ EmpyreanEyes/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  EmpyreanEyes
+//
+//  Created by Sky Wang on 7/6/17.
+//  Copyright © 2017 Sky Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusItem.title = "EmpyreanEyes"
+        statusItem.menu = statusMenu
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
