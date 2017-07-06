780cf2073db51e6f2d6624354ebb87cbfdb1e1cf
skywang329 <sky.wang@yahoo.com>
2017-07-07 02:18:21 +0800

    added location manual update button currently logs to console current location this cllocationmanager is wonky af

--- EmpyreanEyes/StatusMenuController.swift
+++ EmpyreanEyes/StatusMenuController.swift
@@ -7,14 +7,22 @@
 //
 
 import Cocoa
+import CoreLocation
 
-class StatusMenuController: NSObject {
+class StatusMenuController: NSObject, CLLocationManagerDelegate {
     
     @IBOutlet weak var statusMenu: NSMenu!
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    var locationManager:CLLocationManager!
+    var currentLocation:CLLocation?
+    //var count = 0
     
     @IBAction func quitClicked(_ sender: NSMenuItem) {
         NSApplication.shared().terminate(self)
+    }
+    
+    @IBAction func updateClicked(_ sender: NSMenuItem) {
+        updateLocation()
     }
     
     override func awakeFromNib() {
@@ -24,6 +32,39 @@ class StatusMenuController: NSObject {
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
+        setupLocationManager()
+    }
+    
+    func updateLocation() {
+        self.locationManager.delegate = self
+        //self.locationManager.startMonitoringSignificantLocationChanges()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    func setupLocationManager(){
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        //self.locationManager?.requestAlwaysAuthorization()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager?.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.last
+        //locationManager?.stopMonitoringSignificantLocationChanges()
+        let locationValue:CLLocationCoordinate2D = manager.location!.coordinate
+            
+        print(locationValue)//Remember to change this thingy to get RA DEC
+        //count += 1
+        //print(count)
+        
+        locationManager?.stopUpdatingLocation()
+        manager.delegate = nil
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
     }
 
 }
