52bf62a3a84f4c860bf9682eb2dc4b802b05ae19
skywang329 <sky.wang@yahoo.com>
2017-07-07 19:41:20 +0800

    auto get screen reso and url good

--- EmpyreanEyes/StatusMenuController.swift
+++ EmpyreanEyes/StatusMenuController.swift
@@ -9,6 +9,43 @@
 import Cocoa
 import CoreLocation
 
+extension CLLocationCoordinate2D {
+    var latitudeMinutes:  Double {
+        return latitude.multiplied(by: 3600)
+            .truncatingRemainder(dividingBy: 3600)
+            .divided(by: 60)
+    }
+    var latitudeSeconds:  Double {
+        return latitude.multiplied(by: 3600)
+            .truncatingRemainder(dividingBy: 3600)
+            .truncatingRemainder(dividingBy: 60)
+    }
+    
+    var longitudeMinutes: Double {
+        return longitude.multiplied(by: 3600)
+            .truncatingRemainder(dividingBy: 3600)
+            .divided(by: 60)
+    }
+    var longitudeSeconds: Double {
+        return longitude.multiplied(by: 3600)
+            .truncatingRemainder(dividingBy: 3600)
+            .truncatingRemainder(dividingBy: 60)
+    }
+    
+    var dms:(latitude: String, longitude: String) {
+        return (String(format:"%d° %d' %.4f\" %@",
+                       Int(abs(latitude)),
+                       Int(abs(latitudeMinutes)),
+                       abs(latitudeSeconds),
+                       latitude >= 0 ? "N" : "S"),
+                String(format:"%d° %d' %.4f\" %@",
+                       Int(abs(longitude)),
+                       Int(abs(longitudeMinutes)),
+                       abs(longitudeSeconds),
+                       longitude >= 0 ? "E" : "W"))
+    }
+}
+
 class StatusMenuController: NSObject, CLLocationManagerDelegate {
     
     @IBOutlet weak var statusMenu: NSMenu!
@@ -16,6 +53,14 @@ class StatusMenuController: NSObject, CLLocationManagerDelegate {
     var locationManager:CLLocationManager!
     var currentLocation:CLLocation?
     //var count = 0
+    var ra:String = ""
+    var dec:String = ""
+    var astroPicURL:URL!
+    //
+    var screen:NSScreen!
+    var rect:NSRect!
+    var screenHeight:String = ""
+    var screenWidth:String = ""
     
     @IBAction func quitClicked(_ sender: NSMenuItem) {
         NSApplication.shared().terminate(self)
@@ -32,6 +77,11 @@ class StatusMenuController: NSObject, CLLocationManagerDelegate {
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
+        //Init screen resolution size
+        screen = NSScreen.main()!
+        rect = screen.frame
+        screenHeight = String(Int(rect.size.height))
+        screenWidth = String(Int(rect.size.width))
         setupLocationManager()
     }
     
@@ -54,10 +104,22 @@ class StatusMenuController: NSObject, CLLocationManagerDelegate {
         currentLocation = locations.last
         //locationManager?.stopMonitoringSignificantLocationChanges()
         let locationValue:CLLocationCoordinate2D = manager.location!.coordinate
-            
-        print(locationValue)//Remember to change this thingy to get RA DEC
+        
+        //print(locationValue.latitude)//String
+        //print(locationValue.dms.latitude)//Remember to change this thingy to get RA DEC
+        //print(locationValue.longitude)
+        //print(locationValue.dms.longitude)
         //count += 1
         //print(count)
+        
+        //INSERT RA DEC CONVERSION METHOD HERE
+        
+        ra = String(locationValue.longitude) //CHANGE THIS WHEN GET RA DEC
+        dec = String(locationValue.latitude) //CHANGE THIS WHEN GET RA DEC
+        
+        astroPicURL = URL(string: "http://skyserver.sdss.org/dr13/SkyServerWS/ImgCutout/getjpeg?ra=" + ra + "&dec=" + dec + "&scale=0.79224&width=" + screenWidth + "&height=" + screenHeight)
+        
+        print(astroPicURL)
         
         locationManager?.stopUpdatingLocation()
         manager.delegate = nil
