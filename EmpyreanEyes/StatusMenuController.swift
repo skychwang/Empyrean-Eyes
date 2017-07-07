739f34275ec302c8a6b620fa79ac988a7e39dbd2
skywang329 <sky.wang@yahoo.com>
2017-07-08 00:04:36 +0800

    ra dec determination added WOOO THAT WAS HARD LMAO has a offset of like 30 minutes compared to USNO thing so meh; added some error messages in console; cleaned up code; WHEEEEE

--- EmpyreanEyes/StatusMenuController.swift
+++ EmpyreanEyes/StatusMenuController.swift
@@ -72,12 +72,10 @@ class StatusMenuController: NSObject, CLLocationManagerDelegate {
     var ra:String = ""
     var dec:String = ""
     var astroPicURL:URL!
-    //
     var screen:NSScreen!
     var rect:NSRect!
     var screenHeight:String = ""
     var screenWidth:String = ""
-    //
     var image:NSImage!
     var data:Data!
     var workspace = NSWorkspace.shared()
@@ -88,6 +86,16 @@ class StatusMenuController: NSObject, CLLocationManagerDelegate {
     
     @IBAction func updateClicked(_ sender: NSMenuItem) {
         updateLocation()
+    }
+    
+    func jdFromDate(date : NSDate) -> Double {
+        let JD_JAN_1_1970_0000GMT = 2440587.5
+        return JD_JAN_1_1970_0000GMT + date.timeIntervalSince1970 / 86400
+    }
+    
+    func dateFromJd(jd : Double) -> NSDate {
+        let JD_JAN_1_1970_0000GMT = 2440587.5
+        return  NSDate(timeIntervalSince1970: (jd - JD_JAN_1_1970_0000GMT) * 86400)
     }
     
     override func awakeFromNib() {
@@ -107,14 +115,12 @@ class StatusMenuController: NSObject, CLLocationManagerDelegate {
     
     func updateLocation() {
         self.locationManager.delegate = self
-        //self.locationManager.startMonitoringSignificantLocationChanges()
         self.locationManager.startUpdatingLocation()
     }
     
     func setupLocationManager(){
         locationManager = CLLocationManager()
         locationManager?.delegate = self
-        //self.locationManager?.requestAlwaysAuthorization()
         locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager?.startUpdatingLocation()
         
@@ -122,51 +128,76 @@ class StatusMenuController: NSObject, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         currentLocation = locations.last
-        //locationManager?.stopMonitoringSignificantLocationChanges()
         let locationValue:CLLocationCoordinate2D = manager.location!.coordinate
         
-        //print(locationValue.latitude)//String
-        //print(locationValue.dms.latitude)//Remember to change this thingy to get RA DEC
-        //print(locationValue.longitude)
-        //print(locationValue.dms.longitude)
-        //count += 1
-        //print(count)
+        //RA of Zenith Conversion Method From Longitude
         
-        //INSERT RA DEC CONVERSION METHOD HERE
+        let locale = NSTimeZone.init(abbreviation: "UTC")
+        NSTimeZone.default = locale as! TimeZone
         
-        ra = String(locationValue.longitude) //CHANGE THIS WHEN GET RA DEC
-        dec = String(locationValue.latitude) //CHANGE THIS WHEN GET RA DEC
+        let gregorian = Calendar(identifier: .gregorian)
+        let now = Date()
+        var components = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        
+        components.hour = 0
+        components.minute = 0
+        components.second = 0
+        
+        let date0h = gregorian.date(from: components)!
+        
+        components.hour = 24
+        
+        let date0hJD = jdFromDate(date: date0h as NSDate)
+        let nowJD = jdFromDate(date: now as NSDate)
+        
+        let D = nowJD - 2451545.0
+        let D0 = date0hJD - 2451545.0
+        
+        let H = Double(gregorian.component(.hour, from: now))
+        
+        let GMST = 6.697374558 + (0.06570982441908 * D0) + (1.00273790935 * H) + (0.000026 * (D/36525) * (D/36525))
+        
+        let GAST = GMST + (((-0.000319 * sin(125.04 - (0.052954 * D))) - (0.000024 * sin(2 * (280.47 + (0.98565 * D))))) * cos(23.4393 - (0.0000004 * D)))
+        
+        ra = String(GAST.truncatingRemainder(dividingBy: 24) + (locationValue.longitude / 15))
+        //ra = String(GMST.truncatingRemainder(dividingBy: 24) + (locationValue.longitude / 15)) //GMST seems to be closer to value from USNO but GAST is supposed to be the correct value
+        dec = String(locationValue.latitude) //The declination at the zenith is equal to the site's latitude; therefore, the zenith for an observer at 45°N will be +45°.
         
         astroPicURL = URL(string: "https://skyserver.sdss.org/dr13/SkyServerWS/ImgCutout/getjpeg?ra=" + ra + "&dec=" + dec + "&scale=0.79224&width=" + screenWidth + "&height=" + screenHeight)
         
-        print(astroPicURL)
-        
-        //image = NSImage(byReferencing: astroPicURL)
-        //print(image)
-        
         do {
             data = try Data(contentsOf: astroPicURL)
+            image = NSImage(data: data)
+            print("Image fetched.")
+            
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            
+            let destinationURL = documentsURL.appendingPathComponent("EmpyreanEyesImage.png")
+            
+            if image.pngWrite(to: destinationURL) {
+                print("File saved.")
+            }
+            
+            do {
+                try workspace.setDesktopImageURL(destinationURL, for: screen, options: [:])
+                print("Desktop Changed.")
+            } catch {
+                print("--------------------")
+                print("ALERT: Cannot change desktop image.")
+                print("ERROR: ")
+                print(error)
+                print("--------------------")
+                //Add some error stuff in statusbar window if cannot fetch image
+            }
+
         } catch {
+            print("--------------------")
+            print("ALERT: Cannot get png from url: ")
+            print(astroPicURL)
+            print("ERROR: ")
             print(error)
-        }
-        
-        //print(data)
-        
-        image = NSImage(data: data)
-        //print(image)
-        
-        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        
-        let destinationURL = documentsURL.appendingPathComponent("EmpyreanEyesImage.png")
-        if image.pngWrite(to: destinationURL) {
-            print("File saved")
-        }
-        
-        do {
-            try workspace.setDesktopImageURL(destinationURL, for: screen, options: [:])
-            print("Desktop Changed")
-        } catch {
-            print(error)
+            print("--------------------")
+            //Add some error stuff in statusbar window if cannot fetch image
         }
         
         locationManager?.stopUpdatingLocation()
